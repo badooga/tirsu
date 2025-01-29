52bd8c5f0e081fff28b7465f26d969292a515428
badooga <33851191+badooga@users.noreply.github.com>
2025-01-29 03:00:33 -0500

    change orientation to be a fixture

--- tirsu/language/test/test_tir.py
+++ tirsu/language/test/test_tir.py
@@ -19,25 +19,28 @@ class TestTirStr:
             assert alphabet.in_alphabet(letter)
             assert 1 <= len(letter) <= 2
 
-class TestTirWord:
-    @pytest.fixture(
-        scope="class", params=["vlaakith", "vo'n'fynh", "vo'n'ash"]
-    )
-    def word(self, request: pytest.FixtureRequest) -> tir.TirWord:
-        word = request.param
-        return tir.TirWord(word)
 
-    @pytest.mark.parametrize("orientation", [1, -1])
-    def test_angles(self, word: tir.TirWord, orientation: Literal[-1, 1]) -> None:
-        word.orientation = orientation
+class TestTirWord:
+    @pytest.fixture(scope="class", params=[1, -1])
+    def orientation(self, request: pytest.FixtureRequest) -> Literal[-1, 1]:
+        return request.param
+
+    @pytest.fixture(scope="class", params=["vlaakith", "vo'n'fynh", "vo'n'ash"])
+    def word(
+        self, request: pytest.FixtureRequest, orientation: Literal[-1, 1]
+    ) -> tir.TirWord:
+        word = request.param
+        return tir.TirWord(word, orientation)
+
+    def test_angles(self, word: tir.TirWord) -> None:
         n = len(word)
 
-        shifted = np.pi/2 - word.orientation * word.angles
+        shifted = np.pi / 2 - word.orientation * word.angles
         actual = np.exp(1j * shifted)
 
         np.testing.assert_almost_equal(actual**n, 1)
 
-        unity = np.roots([-1, *(0 for _ in range(n-1)), 1])
+        unity = np.roots([-1, *(0 for _ in range(n - 1)), 1])
         unity = unity[np.argsort(np.angle(unity) % (2 * np.pi))]
 
         np.testing.assert_almost_equal(actual, unity)
