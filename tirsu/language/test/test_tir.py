2742b5002fed609f6da45c3d5e676f230991099c
badooga <33851191+badooga@users.noreply.github.com>
2025-01-29 02:56:14 -0500

    add TestTirWord

--- tirsu/language/test/test_tir.py
+++ tirsu/language/test/test_tir.py
@@ -1,4 +1,7 @@
 import pytest
+from typing import Literal
+
+import numpy as np
 
 from .. import alphabet, tir
 
@@ -11,7 +14,30 @@ class TestTirStr:
         word = request.param
         return tir.TirStr(word)
 
-    def test_letters(self, phrase: tir.TirStr):
+    def test_letters(self, phrase: tir.TirStr) -> None:
         for letter in phrase.letters:
             assert alphabet.in_alphabet(letter)
             assert 1 <= len(letter) <= 2
+
+class TestTirWord:
+    @pytest.fixture(
+        scope="class", params=["vlaakith", "vo'n'fynh", "vo'n'ash"]
+    )
+    def word(self, request: pytest.FixtureRequest) -> tir.TirWord:
+        word = request.param
+        return tir.TirWord(word)
+
+    @pytest.mark.parametrize("orientation", [1, -1])
+    def test_angles(self, word: tir.TirWord, orientation: Literal[-1, 1]) -> None:
+        word.orientation = orientation
+        n = len(word)
+
+        shifted = np.pi/2 - word.orientation * word.angles
+        actual = np.exp(1j * shifted)
+
+        np.testing.assert_almost_equal(actual**n, 1)
+
+        unity = np.roots([-1, *(0 for _ in range(n-1)), 1])
+        unity = unity[np.argsort(np.angle(unity) % (2 * np.pi))]
+
+        np.testing.assert_almost_equal(actual, unity)
