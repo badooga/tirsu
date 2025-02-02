46fc0d09f6501fe27004bcd6e3723b4f6077fcb5
badooga <33851191+badooga@users.noreply.github.com>
2025-02-01 21:28:24 -0500

    fix ea properly

--- tirsu/draw/letters.py
+++ tirsu/draw/letters.py
@@ -133,7 +133,7 @@ class DrawLetter(DrawShape):  # pylint: disable=too-many-public-methods
         self.vbar(self.r2 - 7.5, self.delta)
 
     def ea(self) -> None:
-        self.e()
+        self.e(0.25)
         self.a()
 
     def apostrophe(self) -> None:
